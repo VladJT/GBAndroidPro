8ea6d75478287fa5a2f306988b56823c10a89e8a
VladJT <onlyrus@hotmail.com>
2023-03-23 13:22:51 +0300

    исправлен конструктор MainDispatcherRule

--- app/src/test/java/jt/projects/gbandroidpro/MainDispatcherRule.kt
+++ app/src/test/java/jt/projects/gbandroidpro/MainDispatcherRule.kt
@@ -10,8 +10,8 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class MainDispatcherRule : TestWatcher() {
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+class MainDispatcherRule(private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()) :
+    TestWatcher() {
 
     override fun starting(description: Description) {
         Dispatchers.setMain(testDispatcher)
